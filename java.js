9521f854566877f46f1a9eea48012f35d912f23f
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2018-06-30 22:45:17 -0400

    fourth

--- java.js
+++ java.js
@@ -25,37 +25,37 @@ var questions = [
             'Your family was on my list',
             'What is dead may never die'],
         correctAnswer: 'Tell them Winter came for house Frey',
-        video: "<iframe width='560' height='315' src='https://www.youtube.com/embed/0miGSjYG0G4' frameborder='0' allow='autoplay; encrypted-media'allowfullscreen></iframe>"
+        video: "<iframe width='560' height='315' class='img-fluid' src='https://www.youtube.com/embed/0miGSjYG0G4' frameborder='0' allow='autoplay; encrypted-media'allowfullscreen></iframe>"
     },
     {
         question: "What bold action does Jamie take right before the very end of season 8?",
         answers: ["Helps Cersei trick Denarys and Jon", "Rides North to join Denarys and Jon", "Admits his love for Brienne of Tarth", "Brings Marcella home"],
         correctAnswer: "Rides North to join Denarys and Jon",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jOMa0LeWArE?start=232" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/jOMa0LeWArE?start=232" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Who was the first person on Arya Stark's list?",
         answers: ["Joffrey Baratheon", "Jamie Lannister", "Meryn Trant", "Cersei Lannister"],
         correctAnswer: "Meryn Trant",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Q4O9IWLoABU?start=54" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/Q4O9IWLoABU?start=54" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "During the war of the 5 kings, which 'king' was killed by his own brother?",
         answers: ["Stannis Baratheon", "Joffrey Baratheon", "Balon Greyjoy", "Tywin Lannister"],
         correctAnswer: "Balon Greyjoy",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/KCMM0ir8-EA?start=128" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/KCMM0ir8-EA?start=128" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "In which city does the Targaryen bloodline originate?",
         answers: ["Dragonstone", "Pentos", "Valyria", "Essos"],
         correctAnswer: "Valyria",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/afzEd-fCWq0?start=165" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/afzEd-fCWq0?start=165" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
     },
     {
         question: "Who are the Starks ancestors?",
         answers: ["The Children", "The First Men", "The Andals", "The Ironborne"],
         correctAnswer: "The First Men",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tuf0-ZHsOs8?start=85" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/Tuf0-ZHsOs8?start=85" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "What are the names of Denary's 3 dragons?",
@@ -64,55 +64,55 @@ var questions = [
             'Raehgar, Draegon, Viserieon',
             'Drogon, Rhaegal, Viserion'],
         correctAnswer: 'Drogon, Rhaegal, Viserion',
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VvtVK3GxoO4?start=124" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/VvtVK3GxoO4?start=124" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Who put the poison in Jon Arryn's drink?",
         answers: ["Lysa Arryn", "The Mountain", "Cersei Lannister", "Joffrey Baratheon"],
         correctAnswer: "Lysa Arryn",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/qafMsdPLDhM?start=319" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/qafMsdPLDhM?start=319" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Who killed Ned Stark's brother and father?",
         answers: ["Tywin Lannister", "Raeghar Targaryen", "Euron Greyjoy", "Aerys Targaryen"],
         correctAnswer: "Aerys Targaryen",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jRQkt2wqaCE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/jRQkt2wqaCE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Who served as Ser Barriston's squire at one point?",
         answers: ["Jamie Lannister", "Podrick", "Gendry Baratheon", "Robert Baratheon"],
         correctAnswer: ["Jamie Lannister"],
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/1ke9iUJykiw?start=178" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/1ke9iUJykiw?start=178" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Where did Tyrion first meet Bronn",
         answers: ["The Eyrie", "Harrenhall", "A small tavern", "A fight with wildwings"],
         correctAnswer: "A small tavern",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/0cyEM57UJks" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/0cyEM57UJks" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "What is the name of Stannis Baratheon's daughter?",
         answers: ["Princess Mereen", "Princess Beatrice", "Princess Diana", "Princess Shireen"],
         correctAnswer: "Princess Shireen",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/cKp8_hC6gvI?start=43" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/cKp8_hC6gvI?start=43" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "What does 'Khaleesi' translate to in Dothraki?",
         answers: ["Queen", "Princess", "Wife of Khal", "Mother"],
         correctAnswer: "Wife of Khal",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oBFBeGt6Njk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/oBFBeGt6Njk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "Which of the following is not a face of the 7?",
         answers: ["Mother", "Maiden", "Widow", "Crone"],
         correctAnswer: "Widow",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yJZm-9nITWQ?start=10" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/yJZm-9nITWQ?start=10" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     },
     {
         question: "What did The Mountain killing The Viper cause?",
         answers: ["Prince Oberyn's brother to become the Lord of Dorne", "Tyrion to go to prison", "Tyrion to be found guilty of regicide", "The Mountain to go to prison"],
         correctAnswer: "Tyrion to be found guilty of regicide",
-        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VM9wWtHozCM?start=90" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        video: '<iframe width="560" height="315" class="img-fluid" src="https://www.youtube.com/embed/VM9wWtHozCM?start=90" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     }
 
 ];
